5640994b5c03f01f01573dd41e2da3959c50962e
Yudhistira Septian Sampoerno <yudissptn@gmail.com>
2022-05-26 21:22:38 +0700

    add header and about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,49 @@
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
+import { images } from "../../constants";
 import "./About.scss";
 
 const About = () => {
-  return <div>About</div>;
+  const about = [
+    {
+      title: "Web Development",
+      description: "I am a good web developer",
+      imgUrl: images.about01,
+    },
+    {
+      title: "Web Design",
+      description: "I am a good web design",
+      imgUrl: images.about02,
+    },
+  ];
+  return (
+    <>
+      <h2 className="head-text">
+        I Know That <span>Good Design</span>
+        <br /> means <span>Good Business</span>{" "}
+      </h2>
+      <div className="app__profiles">
+        {about.map((item, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profiles-item"
+            key={about.title + index}
+          >
+            <img src={item.imgUrl} alt={item.title}></img>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {item.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {item.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,63 @@
+import { motion } from "framer-motion";
+
+import { images } from "../../constants";
 import "./Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  const scaleVariants = {
+    whileInView: {
+      scale: [0, 1],
+      opacity: [0, 1],
+      transition: { duration: 1, ease: "easeInOut" },
+    },
+  };
+  return (
+    <div id="home" className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Yudhis</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+          </div>
+        </div>
+      </motion.div>
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg"></img>
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="overlay_circle"
+          src={images.circle}
+          alt="profile_circle"
+        />
+      </motion.div>
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circle"
+      >
+        {[images.flutter, images.redux, images.sass].map((item) => (
+          <div className="circle-cmp app__flex" key={`circle-${item}`}>
+            <img src={item} alt="circle"></img>
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
 };
 
 export default Header;
